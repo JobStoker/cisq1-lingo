da1c25977a128f49d3f4f4659137c7816757d8d1
Job Stoker <job@Jobs-MacBook-Pro.local>
2022-01-15 12:48:37 +0100

    Removed gamerepository from initial commit

--- src/main/java/nl/hu/cisq1/lingo/data/SpringGameRepository.java
+++
@@ -1,6 +0,0 @@
-package nl.hu.cisq1.lingo.data;
-
-import nl.hu.cisq1.lingo.trainer.domain.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface SpringGameRepository extends JpaRepository<Game, Long> {
-}
--- src/main/java/nl/hu/cisq1/lingo/trainer/application/GameService.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/application/GameService.java
@@ -1,18 +1,15 @@
 package nl.hu.cisq1.lingo.trainer.application;
 
-import nl.hu.cisq1.lingo.data.SpringGameRepository;
 import nl.hu.cisq1.lingo.trainer.domain.Game;
 import nl.hu.cisq1.lingo.trainer.domain.Progress;
 import nl.hu.cisq1.lingo.words.application.WordService;
 
 public class GameService {
-    private SpringGameRepository gameRepository;
     private WordService wordService;
 
     private Game game;
 
-    public GameService(SpringGameRepository gameRepository, WordService wordService) {
-        this.gameRepository = gameRepository;
+    public GameService(WordService wordService) {
         this.wordService = wordService;
     }
 
@@ -20,7 +17,6 @@ public class GameService {
         this.game = new Game();
 
         this.game.newRound(wordService);
-        this.gameRepository.save(this.game);
 
         return this.game.createProgress();
     }
